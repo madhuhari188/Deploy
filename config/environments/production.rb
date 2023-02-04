884a4fb4a0cd68f518c467fe2c04d57664be8a70
madhuhari188 <madhu.hari.188@gmail.com>
2023-02-04 10:44:21 +0530

    depoly-2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'tech-savvy.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'techsavvy.onrender.com' }
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
